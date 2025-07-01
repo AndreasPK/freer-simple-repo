e98a2f280cea1c93b20fc49d09ef49037265a865
Andreas Klebinger <klebinger.andreas@gmx.at>
2025-07-01 19:02:26 +0200

    no more lazy let

--- bench/Main.hs
+++ bench/Main.hs
@@ -16,6 +16,6 @@ main = go countDownEff 10000 10000
   where
     go :: (Int -> (Int,Int)) -> Int -> Int -> IO ()
     go f 0 x = print (f x) >> print "Done"
-    go f n x = let (!_,!_) = f x
+    go f n x = let !(!_,!_) = f x
                in go f (n-1) x
 
